b840383838b173670b29cea993cbd1b1bebdca64
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-05-30 02:13:29 +0700

    UI/UX for registration and login update

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -2,44 +2,131 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Link } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 
+// Validation schema
+const loginSchema = z.object({
+  email: z.string().email({
+    message: "Invalid email address",
+  }),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(12, "Password must be at most 12 characters"),
+});
 export function LoginForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"div">) {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  function onSubmit(values: z.infer<typeof loginSchema>) {
+    console.log(values);
+    // submit(values, { method: "post", action: "/login" });
+  }
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <form>
-        <div className="flex flex-col gap-6">
-          <div className="flex flex-col items-center gap-2">
-            <h1 className="text-xl font-bold">Log In</h1>
-          </div>
+      <Form {...form}>
+        <form
+          className="md:w-[250px] w-full"
+          onSubmit={form.handleSubmit(onSubmit)}
+          autoComplete="off"
+        >
           <div className="flex flex-col gap-6">
-            <div className="grid gap-2">
-              <Input id="email" type="email" placeholder="Email" required />
+            <div className="flex flex-col items-center gap-2">
+              <h1 className="text-xl font-bold">Log In</h1>
             </div>
-            <Button type="submit" className="w-full bg-primary_1">
-              Login
-            </Button>
-          </div>
-          <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-            <span className="relative z-10 bg-background px-2 text-muted-foreground">
-              Or Continue with
-            </span>
-          </div>
-
-          <div className="flex justify-center">
-            <Button variant="ghost" className="">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path
-                  d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                  fill="currentColor"
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          id="email"
+                          type="email"
+                          autoComplete="off"
+                          placeholder="Email"
+                          required
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
                 />
-              </svg>
-            </Button>
+              </div>
+
+              <div className="grid gap-2">
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          id="password"
+                          type="password"
+                          placeholder="password"
+                          autoComplete="off"
+                          required
+                          {...field}
+                        />
+                      </FormControl>
+                      <div className="flex items-center">
+                        <a
+                          href="#"
+                          className="ml-auto inline-block text-xs underline-offset-4 hover:underline"
+                        >
+                          Forgot your password?
+                        </a>
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <Button type="submit" className="w-full bg-primary_1">
+                Login
+              </Button>
+            </div>
+
+            <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+              <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                Or Continue with
+              </span>
+            </div>
+
+            <div className="flex justify-center">
+              <Button variant="ghost" className="">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path
+                    d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </Button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </Form>
 
       <div className="text-center text-sm">
         Don&apos;t have an account?{" "}
--- src/components/auth/register-form.tsx
+++ src/components/auth/register-form.tsx
@@ -9,17 +9,36 @@ export function RegisterForm({
 }: React.ComponentPropsWithoutRef<"div">) {
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <form>
+      <form className="md:w-[250px] w-full" autoComplete="off">
         <div className="flex flex-col gap-6">
           <div className="flex flex-col items-center gap-2">
             <h1 className="text-xl font-bold">Registration</h1>
           </div>
           <div className="flex flex-col gap-6">
             <div className="grid gap-2">
+              <Input id="name" type="text" placeholder="Username" required />
+            </div>
+            <div className="grid gap-2">
               <Input id="email" type="email" placeholder="Email" required />
             </div>
-            <Button type="submit" className="w-full">
-              Login
+            <div className="grid gap-2">
+              <Input
+                id="password"
+                type="password"
+                placeholder="Password"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Input
+                id=""
+                type="password"
+                placeholder="Reconfirm password"
+                required
+              />
+            </div>
+            <Button type="submit" className="w-full bg-primary_1">
+              Sign Up
             </Button>
           </div>
           <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
@@ -29,7 +48,7 @@ export function RegisterForm({
           </div>
 
           <div className="flex justify-center">
-            <Button variant="outline" className="w-full">
+            <Button variant="ghost" className="">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path
                   d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
@@ -42,9 +61,9 @@ export function RegisterForm({
       </form>
 
       <div className="text-center text-sm">
-        Don&apos;t have an account?{" "}
-        <Link to="/register" className="underline underline-offset-4">
-          Sign up
+        Already have an account?{" "}
+        <Link to="/login" className="underline underline-offset-4">
+          Sign In
         </Link>
       </div>
     </div>
